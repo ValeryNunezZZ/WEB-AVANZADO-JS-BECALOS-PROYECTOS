6ef59693b630b01dbee3c543048a23553b577857
ValeryNunezBrow <nunez.ramirez.valery.aylin@gmail.com>
2025-05-30 07:16:30 -0600

    Añadiendo proyecto realizado con NODE JS

---
+++ PROYECTOS_DEV/CLASE_7/mi-exploracion-espacial/index.js
@@ -0,0 +1,12 @@
+const planetas = require('./planetas')
+
+planetas.forEach(planeta => {
+    console.log(`¡Planeta ${planeta.nombre} descubierto!`);
+    console.log(`Descripción: ${planeta.descripcion}`);
+    console.log(`Descubierto en: ${planeta.descubiertoEn}`);
+    console.log('---');
+});
+
+const cowsay = require('cowsay');
+
+console.log(cowsay.say({ text: "Hello, PLANETAS!" }));
---
+++ PROYECTOS_DEV/CLASE_7/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,46 @@
+const planetas = [
+    {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+    },
+    {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+    },
+    {
+    nombre: "Kepler-452b",
+    descripcion: "Un exoplaneta similar a la Tierra en la zona habitable de una estrella parecida al Sol.",
+    descubiertoEn: "2015"
+    },
+    {
+    nombre: "Trappist-1e",
+    descripcion: "Uno de los siete exoplanetas en el sistema TRAPPIST-1, con potencial para agua líquida.",
+    descubiertoEn: "2017"
+    },
+    {
+    nombre: "Ganímedes",
+    descripcion: "La luna más grande del Sistema Solar, más grande que Mercurio.",
+    descubiertoEn: "1610"
+    },
+    {
+    nombre: "Europa",
+    descripcion: "Una luna de Júpiter con un océano subterráneo bajo su superficie helada.",
+    descubiertoEn: "1610"
+    },
+    {
+    nombre: "Gliese 581g",
+    descripcion: "Un exoplaneta potencialmente habitable orbitando una estrella enana roja.",
+    descubiertoEn: "2010"
+    },
+    {
+    nombre: "HD 209458 b",
+    descripcion: "Un exoplaneta gaseoso con atmósfera detectada, conocido como Osiris.",
+    descubiertoEn: "1999"
+    }
+];
+    
+
+//NECESARIO PARA IMPORTAR EN APP ESTE JSON
+module.exports = planetas;
